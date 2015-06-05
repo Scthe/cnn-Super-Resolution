442b186f79f9b2779d3749923aba2000b5e6ce2c
Scthe <marcin1113C@gmail.com>
2015-06-05 18:28:06 +0200

    Use json test data in python script

--- test/test_confirmation.py
+++ test/test_confirmation.py
@@ -1,5 +1,6 @@
 import math
-
+import json
+from pprint import pprint
 #
 # This file contains python version of code in opencl kernels.
 # It's purpose is to double check the test values.
@@ -15,12 +16,6 @@ def format_arr(arr, comma_places=3):
   return ', '.join([("%."+str(comma_places)+"f") % x for x in arr])
 
 class Layer1:
-  __in = [0.0,     255.0,   207.073, 217.543, 111.446,
-          43.246,  178.755, 105.315, 225.93,  200.578,
-          109.577, 76.245,  149.685, 29.07,   180.345,
-          170.892, 190.035, 217.543, 190.035, 76.278,
-          205.58,  149.852, 218.917, 151.138, 179.001]
-
   '''
   filter 1:
   []__[]
@@ -36,54 +31,47 @@ class Layer1:
   ______
   __[]__
   ______
-
-  (read the numbers by columns)
   '''
-  _w = [1, 0, 0,
-        0, 1, 0,
-        1, 0, 0,
-        0, 1, 0,
-        1, 0, 1,
-        0, 1, 0,
-        1, 0, 0,
-        0, 1, 0,
-        1, 0, 0];
-  _b = [0.1, 0.2, 0.3]
 
-  def preprocess(self, data):
-    a1 = [x / 255.0 for x in data]
-    mean = sum(a1) / len(a1)
-    return [x - mean for x in a1]
-
-  def __call__(self):
+  def __call__(self, cfg):
     print('### LAYER 1')
-    _in = self.preprocess(self.__in)
-    result = [ [[]] * 3,[[]] * 3,[[]] * 3]
 
-    for i in range(9):
-      x = i % 3 + 1
-      y = i // 3 + 1
+    raw_in = cfg['input']
+    mean = sum(raw_in) / len(raw_in)
+    _in = [x - mean for x in raw_in]
+
+    f1 = cfg['f1']
+    n1 = cfg['n1']
+    in_w = cfg['input_w']
+    weights = cfg['weights']
+    result = [ [[]] * f1,[[]] * f1,[[]] * n1]
+
+    for i in range(f1*f1):
+      x = i % f1 + 1
+      y = i // f1 + 1
       # print(str(x)+":"+str(y))
       pos = x, y
-      padding = 1,1
+      padding = f1//2, f1//2
 
-      filter_vals = [0,0,0]
+      filter_vals = [0] * n1
       # apply weights
-      for dy in range(3):
-        for dx in range(3):
+      for dy in range(f1):
+        for dx in range(f1):
           delta = dx,dy
           pixel_pos = pos[0] + delta[0] - padding[0],\
                       pos[1] + delta[1] - padding[1]
-          pixel_idx = pixel_pos[1] * 5 + pixel_pos[0]
+          pixel_idx = pixel_pos[1] * in_w + pixel_pos[0]
           pixel_value = _in[pixel_idx]
           # print("\t"+str(pixel_pos)+"->"+str(pixel_value))
-          base_W_idx = ((dy * 3) + dx) * 3
-          for filter_id in range(3):
-            W_value = self._w[base_W_idx + filter_id]
+          base_W_idx = ((dy * f1) + dx) * f1
+          for filter_id in range(n1):
+            W_value = weights[base_W_idx + filter_id]
             filter_vals[filter_id] += W_value * pixel_value
 
-      for filter_id in range(3):
-        B_value = self._b[filter_id]
+      # apply bias
+      biases = cfg['bias']
+      for filter_id in range(n1):
+        B_value = biases[filter_id]
         filter_vals[filter_id] += B_value
 
       filter_vals = list(map(sigmoid, filter_vals))
@@ -92,61 +80,59 @@ class Layer1:
     return result
 
 
-
-
 class Layer2:
-  _w1 = [[(1.000,1.001,1.002), # row 0
-          (1.010,1.011,1.012),
-          (1.020,1.021,1.022)],
-         [(1.100,1.101,1.102), # row 1
-          (1.110,1.111,1.112),
-          (1.120,1.121,1.122)],
-         [(1.200,1.201,1.202), # row 2
-          (1.210,1.211,1.212),
-          (1.220,1.221,1.222)]]
 
-  _w2 = [[(2.000,2.001,2.002), # row 0
-          (2.010,2.011,2.012),
-          (2.020,2.021,2.022)],
-         [(2.100,2.101,2.102), # row 1
-          (2.110,2.111,2.112),
-          (2.120,2.121,2.122)],
-         [(2.200,2.201,2.202), # row 2
-          (2.210,2.211,2.212),
-          (2.220,2.221,2.222)]]
-  _b = [0.1, 0.2]
-
-  def __call__(self, _in):
+  def __call__(self, layer_1_cfg, layer_2_cfg):
     print('\n### LAYER 2')
-    sum_ = [0.0,0.0]
 
-    # apply weights
-    for dy in range(3):
-      for dx in range(3):
-        # print(str(dy+1)+":"+str(dx+1)+" = "+format_arr(_in[dy][dx]))
-        # W_mul = sum(self._w1[dy][dx]), sum(self._w2[dy][dx])
-        W_mul = sum([ a*b for a,b in zip(self._w1[dy][dx], _in[dy][dx])]), \
-                sum([ a*b for a,b in zip(self._w2[dy][dx], _in[dy][dx])])
-        # W_mul = sum(_in[dy][dx]), sum(_in[dy][dx])
-        res = W_mul[0], W_mul[1]
-        sum_[0] += res[0]
-        sum_[1] += res[1]
+    f1 = layer_1_cfg['f1']
+    f2 = layer_2_cfg['f2']
+    n1 = layer_1_cfg['n1']
+    n2 = layer_2_cfg['n2']
+    _in = layer_2_cfg['input']
+    weights = layer_2_cfg['weights']
+    bias = layer_2_cfg['bias']
+    img_in = layer_1_cfg['input_w'], layer_1_cfg['input_h']
+    out_dim = img_in[0] - f1 - f2 + 2,\
+              img_in[1] - f1 - f2 + 2
+    # print(out_dim)
 
-    # add bias
-    sum_[0] += self._b[0]
-    sum_[1] += self._b[1]
-    print ("f1: {:.6} f2: {:.6}".format(sum_[0], sum_[1]))
+    for i in range(out_dim[0]*out_dim[1]):
+        x = i % f2 + 1
+        y = i // f2 + 1
+        pos = x, y
+        padding = f2//2, f2//2
+        vals_by_filter = [0.0] * n2
 
-
-
+        # apply weights
+        for dy in range(f2):
+          for dx in range(f2):
+              delta = dx,dy
+              pixel_pos = pos[0] + delta[0] - padding[0],\
+                          pos[1] + delta[1] - padding[1]
+              pixel_idx = pixel_pos[1] * f1 + pixel_pos[0]
+              for i_n1 in range(n1):
+                  point_value = _in[pixel_idx + i_n1]
+                  base_W_idx = ((((dy * f1) + dx) * f1)+i_n1) * n2
+                  for filter_id in range(n2):
+                      vals_by_filter[filter_id] += weights[base_W_idx+filter_id] * point_value
+        # add bias
+        for filter_id in range(n2):
+            vals_by_filter[filter_id] += bias[filter_id]
+        print("{}:{} = {}".format(y, x, format_arr(vals_by_filter)))
 
 if __name__ == '__main__':
-  l1 = Layer1()
-  l2 = Layer2()
+    cfg = None
+    with open('test/data/test_cases.json') as json_file:
+        cfg = json.load(json_file)
+    # pprint(cfg)
 
-  layer2_in = l1()
-  l2(layer2_in)
+    l1 = Layer1()
+    l2 = Layer2()
 
-  # a = [0.0,     255.0,   207.073, 217.543, 111.446,43.246,  178.755, 105.315, 225.93,  200.578,109.577, 76.245,  149.685, 29.07,   180.345,170.892, 190.035, 217.543, 190.035, 76.278, 205.58,  149.852, 218.917, 151.138, 179.001]
-  # a1 = [x / 255.0 for x in a]
-  # print( ','.join(["{:.5}f".format(x) for x in a1]))
+    layer2_in = l1(cfg['layer_1'])
+    l2(cfg['layer_1'], cfg['layer_2']['data_set_1'])
+
+    # a = [0.0,     255.0,   207.073, 217.543, 111.446,43.246,  178.755, 105.315, 225.93,  200.578,109.577, 76.245,  149.685, 29.07,   180.345,170.892, 190.035, 217.543, 190.035, 76.278, 205.58,  149.852, 218.917, 151.138, 179.001]
+    # a1 = [x / 255.0 for x in a]
+    # print( ','.join(["{:.5}f".format(x) for x in a1]))
